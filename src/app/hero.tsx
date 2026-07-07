a7886cf6ad0d37f92f04f3f147b22634ef052562
Angel Tesa <angeltesa11@gmail.com>
2026-07-07 22:52:51 +0700

    feat: update hero section profile picture

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -46,7 +46,7 @@ function Hero() {
           width={1024}
           height={1024}
           alt="Angel Tesalonika - Frontend Developer"
-          src={`${getImagePrefix()}image/image-7.svg`}
+          src={`${getImagePrefix()}image/avatar1.jpg`}
           className="h-[36rem] w-full rounded-xl object-cover"
         />
       </div>
